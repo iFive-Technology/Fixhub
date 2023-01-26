65b7da5d8d8595e3c2bd3ba20c7359ba4a752148
BestieeMu <chideraigboka7@gmail.com>
2023-01-26 14:21:15 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Footer from './Components/Footer/Footer'
-
+import About from './Components/About/About'
+import WhyUs from './Components/Us/WhyUs'
+import Hero from './Components/Hero/Hero'
 
 
 function App() {
