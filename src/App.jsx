ff03940af463d3bf08f8d01966706cd10c031e36
BestieeMu <chideraigboka7@gmail.com>
2023-02-07 00:02:53 +0100

    dashboard updated today monday new

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Dmain from './Eng-Components/D-Main/D-Pages/Home/Dmain';
 import Profile from './Eng-Components/D-Main/D-Pages/Profile/Profile';
 import ProfileSetting from './Eng-Components/D-Main/D-Pages/Profile/Profile-component/ProfileSetting';
 import ProfileLayout from './Eng-Components/D-Main/D-Pages/Profile/ProfileLayout';
+import EngLayout from './Eng-Components/EngLayout';
 
 function App() {
 
@@ -19,17 +20,17 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/Authentication' element={<Auth />}/>
-        <Route path='/enginner' element={<Enginer />} />
-
-        <Route path='/dashboard' element={<Dashboard />} >
-          <Route index element={<Dmain />} />
-          
-          <Route path='profile' element={<ProfileLayout />}>
+        <Route path='/Authentication' element={<Auth />}/>;
+        <Route path='/enginner' element={<EngLayout />}>
+        <Route index element={<Enginer />} />
+        <Route path='dashboard' element={<Dashboard />} >
+          <Route index element={<Dmain />} />  
+        <Route path='profile' element={<ProfileLayout />}>
           <Route index element={<Profile />} />
           <Route path='profile-setting' element={<ProfileSetting />} />
           </Route>
 
+        </Route>
         </Route>
       </Routes>
       
---
+++ src/Eng-Components/EngLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const EngLayout = () => {
+  return (
+    <div>
+        <Outlet />
+    </div>
+  )
+}
+
+export default EngLayout
